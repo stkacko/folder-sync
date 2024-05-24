a98ce90354a654ea10f37a8cdf5e13c79886f538
catfish <stepan.sumec@gmail.com>
2024-05-24 18:32:23 +0200

    style(): remove commented code

--- folder_sync/folder_sync.py
+++ folder_sync/folder_sync.py
@@ -107,23 +107,3 @@ class FolderSync:
         print(log_message)
         with open(self.log_file, "a") as file:
             file.write(log_message)
-
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Synchronize two folders.")
-#     parser.add_argument("source_folder", type=str, help="Path to the source folder.")
-#     parser.add_argument("replica_folder", type=str, help="Path to the replica folder.")
-#     parser.add_argument(
-#         "sync_interval", type=int, help="Synchronization interval in seconds."
-#     )
-#     parser.add_argument("log_file", type=str, help="Path to the log file.")
-#
-#     args = parser.parse_args()
-#
-#     sync = FolderSync(
-#         args.source_folder, args.replica_folder, args.sync_interval, args.log_file
-#     )
-#
-#     while True:
-#         sync.perform_synchronization()
-#         time.sleep(sync.sync_interval)
